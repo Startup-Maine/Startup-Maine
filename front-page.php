7c9670f6a0ab828bee9dac8308bd44b32f9bf389
Josh Reisner <josh@joshreisner.com>
2017-03-06 15:32:48 -0800

    Home page tweaks per kate

--- front-page.php
+++ front-page.php
@@ -18,34 +18,22 @@
 						</div>
 						<div class="item">
 							<div class="image" style="background-image:url(/wp-content/themes/mscw/assets/img/carousel/02-attendees.jpg)"></div>
-							<div class="caption">
+							<div class="caption big">
 								<p>“Entrepreneurs converge on the city to network and learn new skills.”</p>
 								<p>– Portland Press Herald</p>
 							</div>
 						</div>
 						<div class="item">
 							<div class="image" style="background-image:url(/wp-content/themes/mscw/assets/img/carousel/03-lunch.jpg)"></div>
-							<div class="caption">
-								<p>“The variety of workshop sessions was great, I wish I could have gone to all of them. The keynote speakers during lunch were also very informative. I felt like my time was used wisely during MSCW.”</p>
-							</div>
-						</div>
-						<div class="item">
-							<div class="image" style="background-image:url(/wp-content/themes/mscw/assets/img/carousel/04-interview.jpg)"></div>
-							<div class="caption">
-								<p>"It was not academic; the guest speakers are out there doing it! Very upbeat atmosphere."</p>
-							</div>
-						</div>
-						<div class="item">
-							<div class="image" style="background-image:url(/wp-content/themes/mscw/assets/img/carousel/05-panel.jpg)"></div>
-							<div class="caption">
-								<p>One of the best parts of Maine Startup and Create Week? Maine! Come early or stay late to enjoy the legendary Maine summer.</p>
-							</div>
-						</div>
-						<div class="item">
-							<div class="image" style="background-image:url(/wp-content/themes/mscw/assets/img/carousel/06-landscape.png)"></div>
-							<div class="caption">
+							<div class="caption big">
 								<p>“Entrepreneurs converge on the city to network and learn new skills.”</p>
 								<p>– Portland Press Herald</p>
+							</div>
+						</div>
+						<div class="item">
+							<div class="image" style="background-image:url(/wp-content/themes/mscw/assets/img/carousel/04-landscape.png)"></div>
+							<div class="caption big">
+								<p>One of the best parts of Maine Startup and Create Week? Maine! Come early or stay late to enjoy the legendary Maine summer.</p>
 							</div>
 						</div>
 						<div class="item">
@@ -67,25 +55,30 @@
 	<div class="row">
 		<div class="col-md-offset-1 col-md-10">
 			<p class="tagline">
-				<strong>Start something. Solve something. Break a barrier, make a mark.</strong>
-				Join us for a week of expert insight, applied skill-building, and meaningful connections
-				that will help make big things happen.
+				<strong>Start stronger. Scale smarter. Solve better.</strong>
+				Join us for a week of expert insight, applied skill-building, and meaningful connections that will help big things happen.
 			</p>
 			<div class="row testimonials">
-				<?php foreach (array(1, 2) as $num) {?>
 				<div class="col-md-6 testimonial">
 					<div class="row">
 						<div class="col-xs-6">
-							<img src="<?php echo get_stylesheet_directory_uri()?>/assets/img/carousel/01-marvin-ammori.jpg" width="1333" height="1333" class="img-responsive">
+							<img src="<?php echo get_stylesheet_directory_uri()?>/assets/img/testimonials/01-interview.jpg" width="1500" height="1000" class="img-responsive">
 						</div>
 						<div class="col-xs-6">
-							<strong>Testimonial <?php echo $num?></strong>
-							Maine Startup and Create Week is an awesome experience. It really feels intimate and people are willing to help you
-							out with whatever challenge you're facing.
+							“The variety of workshop sessions was great, I wish I could have gone to all of them. The keynote speakers during lunch were also very informative. I felt like my time was used wisely during MSCW.”
 						</div>
 					</div>
 				</div>
-				<?php }?>
+				<div class="col-md-6 testimonial">
+					<div class="row">
+						<div class="col-xs-6">
+							<img src="<?php echo get_stylesheet_directory_uri()?>/assets/img/testimonials/02-panel.jpg" width="1500" height="1000" class="img-responsive">
+						</div>
+						<div class="col-xs-6">
+							“It was not academic; the guest speakers are out there doing it! Very upbeat atmosphere.”
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
