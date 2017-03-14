c8900530c0450c458fead8e53a357dcda63ca984
Josh Reisner <josh@joshreisner.com>
2017-03-13 17:49:22 -0700

    Blog improvements

--- index.php
+++ index.php
@@ -2,18 +2,21 @@
 
 <div class="container">
 	<div class="row">
-		<div class="col-md-offset-1 col-md-10">
+		<div class="col-md-offset-1 col-md-10 blog">
 			<?php 
 			while (have_posts()) {
 				the_post();
 				?>
-				<div class="row post">
-					<div class="col-md-3">
-					</div>
-					<div class="col-md-9">
-						<small><?php the_date('m.d.y')?></small>
-						<h3><?php the_title()?></h3>
-						<?php the_excerpt()?>
+				<div class="post">
+					<div class="row">
+						<div class="col-md-3">
+							<?php echo get_the_post_thumbnail($post->ID, 'large', array('class' => 'img-responsive'))?>
+						</div>
+						<div class="col-md-9">
+							<small><?php the_date('m.d.y')?></small>
+							<h3><?php the_title()?></h3>
+							<?php the_excerpt()?>
+						</div>
 					</div>
 				</div>
 			<?php }?>
