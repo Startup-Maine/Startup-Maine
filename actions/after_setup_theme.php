95a15b796abf5bfd84aa9d2e5e4747354bd77c03
Josh Reisner <josh@joshreisner.com>
2017-03-15 11:43:40 -0700

    Working on typography

--- actions/after_setup_theme.php
+++ actions/after_setup_theme.php
@@ -1,6 +1,11 @@
 <?php
 
+//we want post thumbnails and to be able to manage the document title
 add_theme_support('post-thumbnails');
 add_theme_support('title-tag');
 
+//custom image sizes
 //add_image_size('speakers', 640, 640, true);
+
+//custom editor, using the main stylesheet for this
+add_editor_style(get_stylesheet_uri());
--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,10 @@ const elixir = require('laravel-elixir');
 require('laravel-elixir-vue-2');
 
 elixir((mix) => {
-	mix.sass('./resources/scss/main.scss', './assets/css/main.css')
+	mix.sass('./assets/main.scss', './assets/css/main.css')
 	.scripts([
 		'./node_modules/jquery/dist/jquery.min.js',
 		'./node_modules/bootstrap-sass/assets/javascripts/bootstrap.js',
-		'./resources/js/main.js',
+		'./assets/main.js',
 	], './assets/js/main.js');
 });
