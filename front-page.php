bb8bf922f1be90fe871495114fc34af417be81e4
Josh Reisner <josh@joshreisner.com>
2017-03-28 12:44:27 -0700

    Carousel in the CMS

--- front-page.php
+++ front-page.php
@@ -2,63 +2,44 @@
 
 <div class="hero">
 	<div class="container">
+		<?php
+		if ($items = get_posts(array(
+				'post_type' => 'mscw_carousel',
+				'numberposts' => -1,
+			))) {?>
 		<div class="row">
 			<div class="col-md-offset-1 col-md-10">
-				<div class="keynote">
-					<div class="carousel slide" id="home-carousel" data-ride="carousel">
-						<div class="carousel-inner">
-							<div class="item active">
-								<div class="image" style="background-image:url(<?php echo get_stylesheet_directory_uri()?>/assets/img/carousel/05-mscw-boston.jpg)"></div>
-								<div class="caption big">
-									<p>Maine Startup & Create is coming to Boston! Meet Maine's coolest entrepreneurs, one night only: March 23 at RedThread, Boston.</p>
-									<p class="source"><a href="https://www.eventbrite.com/e/maine-startup-and-create-tour-tickets-31393278140">Get Tickets Now!</a></p>
-								</div>
-							</div>
-							<div class="item">
-								<div class="image" style="background-image:url(<?php echo get_stylesheet_directory_uri()?>/assets/img/carousel/01-marvin-ammori.jpg)"></div>
+				<div class="carousel slide" id="home-carousel" data-ride="carousel">
+					<div class="carousel-inner">
+						<?php foreach ($items as $key => $item) {
+							setup_postdata($item);
+							?>
+						<div class="item<?php if (!$key) echo ' active'?>">
+							<?php if ($image = get_the_post_thumbnail_url($item->ID, 'large')) {?>
+								<div class="image" style="background-image:url(<?php echo $image?>)"></div>
 								<div class="caption">
-									<small>Keynote 1</small>
-									<h3>Marvin Ammori</h3>
-									<h4>General Counsel, Hyperloop One</h4>
-									<p>Named among Politico's 50 visionaries for 2015, Fast Company's 100 Most Creative in Business in 2012, a Washingtonian Magazine "Tech Titan" in 2015, Marvin focuses on next-generation transportation law.</p>
-									<p>“I lead the legal team and serve on the senior business leadership team at Hyperloop One, a company working to make ultra-highspeed ground transportation a reality.”</p>
+									<?php the_content()?>
 								</div>
-							</div>
-							<div class="item">
-								<div class="image" style="background-image:url(<?php echo get_stylesheet_directory_uri()?>/assets/img/carousel/02-attendees.jpg)"></div>
-								<div class="caption big">
-									<p>“Entrepreneurs converge on the city to network and learn new skills.”</p>
-									<p class="source">– Portland Press Herald</p>
-								</div>
-							</div>
-							<div class="item">
-								<div class="image" style="background-image:url(<?php echo get_stylesheet_directory_uri()?>/assets/img/carousel/03-lunch.jpg)"></div>
-								<div class="caption big">
-									<p>75+ Events over 5 days! A game-changing week of thoughtful speakers, passionate discussion, A-ha! moments, and genuine connections.</p>
-								</div>
-							</div>
-							<div class="item">
-								<div class="image" style="background-image:url(<?php echo get_stylesheet_directory_uri()?>/assets/img/carousel/04-landscape.png)"></div>
-								<div class="caption big">
-									<p>One of the best parts of Maine Startup and Create Week? Maine! Come early or stay late to enjoy the legendary Maine summer.</p>
-								</div>
-							</div>
-							<div class="item">
-								<iframe width="945" height="532" src="https://www.youtube.com/embed/0R305euPnSw?enablejsapi=1&rel=0" id="ytplayer" frameborder="0" allowfullscreen></iframe>
-							</div>
+							<?php } else {
+								the_content();
+							}?>
 						</div>
-						<a class="left carousel-control" href="#home-carousel" role="button" data-slide="prev">
-							<span class="fa fa-angle-left" aria-hidden="true"></span>
-							<span class="sr-only">Previous</span>
-						</a>
-						<a class="right carousel-control" href="#home-carousel" role="button" data-slide="next">
-							<span class="fa fa-angle-right" aria-hidden="true"></span>
-							<span class="sr-only">Next</span>
-						</a>
+						<?php }?>
 					</div>
+					<?php if (count($items) > 1) {?>
+					<a class="left carousel-control" href="#home-carousel" role="button" data-slide="prev">
+						<span class="fa fa-angle-left" aria-hidden="true"></span>
+						<span class="sr-only">Previous</span>
+					</a>
+					<a class="right carousel-control" href="#home-carousel" role="button" data-slide="next">
+						<span class="fa fa-angle-right" aria-hidden="true"></span>
+						<span class="sr-only">Next</span>
+					</a>
+					<?php }?>
 				</div>
 			</div>
 		</div>
+		<?php }?>
 		<div class="row">
 			<div class="col-md-offset-1 col-md-10">
 				<p class="tagline">
