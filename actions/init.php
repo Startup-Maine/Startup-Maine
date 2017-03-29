b03abff410b9187bd432f1654ba4f4dee72fe7a5
Josh Reisner <josh@joshreisner.com>
2017-03-29 11:08:03 -0700

    Getting started on program and people

--- actions/init.php
+++ actions/init.php
@@ -4,38 +4,11 @@ register_nav_menu('social', 'Social Menu');
 register_nav_menu('main', 'Header Menu');
 register_nav_menu('footer', 'Tab Menu');
 
-register_post_type('mscw_speaker',
-	array(
-		'labels'		=> array(
-			'name'					=> 'Speakers',
-			'singular_name'			=> 'Speaker',
-			'add_new_item'			=> 'Add Speaker',
-			'edit_item'				=> 'Edit Speaker',
-			'new_item'				=> 'New Speaker',
-			'view_item'				=> 'View Speaker',
-			'view_items'				=> 'View Speakers',
-			'search_items'			=> 'Search Speaker',
-			'not_found'				=> 'No speakers added yet.',
-			'not_found_in_trash'		=> 'No speakers items found in Trash',
-			'all_items'				=> 'All Speakers',
-			'archives'				=> 'Speaker Archives',
-			'attributes'				=> 'Speaker Attributes',
-			'insert_into_item'		=> 'Insert into speaker',
-			'uploaded_to_this_item'	=> 'Uploaded to this speaker',
-			
-		),
-		'supports'		=> array('title', 'thumbnail', 'editor'),
-		'show_ui'		=> true,
-		'has_archive'	=> true,
-		'menu_icon'		=> 'dashicons-groups',
-		'rewrite'		=> array('slug'=>'speakers'),
-	)
-);
-
 register_post_type('mscw_carousel',
 	array(
 		'labels'		=> array(
 			'name'					=> 'Carousel Items',
+			'menu_name'				=> 'Carousel',
 			'singular_name'			=> 'Carousel Item',
 			'add_new_item'			=> 'Add Carousel Item',
 			'edit_item'				=> 'Edit Carousel Item',
@@ -56,7 +29,6 @@ register_post_type('mscw_carousel',
 		'show_ui'		=> true,
 		'has_archive'	=> false,
 		'menu_icon'		=> 'dashicons-images-alt2',
-		//'rewrite'		=> array('slug'=>'speakers'),
 	)
 );
 
@@ -73,7 +45,7 @@ register_post_type('mscw_sponsor',
 			'search_items'			=> 'Search Sponsors',
 			'not_found'				=> 'No sponsors added yet.',
 			'not_found_in_trash'		=> 'No sponsors found in Trash',
-			'all_items'				=> 'All Sponsor',
+			'all_items'				=> 'All Sponsors',
 			'archives'				=> 'Sponsor Archives',
 			'attributes'				=> 'Sponsor Attributes',
 			'insert_into_item'		=> 'Insert into sponsor',
@@ -84,6 +56,64 @@ register_post_type('mscw_sponsor',
 		'show_ui'		=> true,
 		'has_archive'	=> false,
 		'menu_icon'		=> 'dashicons-awards',
-		//'rewrite'		=> array('slug'=>'speakers'),
 	)
 );
+
+register_post_type('mscw_session',
+	array(
+		'labels'		=> array(
+			'name'					=> 'Sessions',
+			'menu_name'				=> 'Program',
+			'singular_name'			=> 'Session',
+			'add_new_item'			=> 'Add Session',
+			'edit_item'				=> 'Edit Session',
+			'new_item'				=> 'New Session',
+			'view_item'				=> 'View Session',
+			'view_items'				=> 'View Sessions',
+			'search_items'			=> 'Search Sessions',
+			'not_found'				=> 'No sessions added yet.',
+			'not_found_in_trash'		=> 'No sessions found in Trash',
+			'all_items'				=> 'All Sessions',
+			'archives'				=> 'Session Archives',
+			'attributes'				=> 'Session Attributes',
+			'insert_into_item'		=> 'Insert into session',
+			'uploaded_to_this_item'	=> 'Uploaded to this session',
+			
+		),
+		'supports'		=> array('title', 'editor'),
+		'show_ui'		=> true,
+		'has_archive'	=> true,
+		'menu_icon'		=> 'dashicons-schedule',
+		'rewrite'		=> array('slug'=>'program'),
+	)
+);
+
+register_post_type('mscw_presenter',
+	array(
+		'labels'		=> array(
+			'name'					=> 'Presenters',
+			'menu_name'				=> 'People',
+			'singular_name'			=> 'Presenter',
+			'add_new_item'			=> 'Add Presenter',
+			'edit_item'				=> 'Edit Presenter',
+			'new_item'				=> 'New Presenter',
+			'view_item'				=> 'View Presenter',
+			'view_items'				=> 'View Presenters',
+			'search_items'			=> 'Search Presenter',
+			'not_found'				=> 'No presenters added yet.',
+			'not_found_in_trash'		=> 'No presenters found in Trash',
+			'all_items'				=> 'All Presenters',
+			'archives'				=> 'Presenter Archives',
+			'attributes'				=> 'Presenter Attributes',
+			'insert_into_item'		=> 'Insert into presenter',
+			'uploaded_to_this_item'	=> 'Uploaded to this presenter',
+			
+		),
+		'supports'		=> array('title', 'thumbnail', 'editor'),
+		'show_ui'		=> true,
+		'has_archive'	=> true,
+		'menu_icon'		=> 'dashicons-groups',
+		'rewrite'		=> array('slug'=>'people'),
+	)
+);
+
