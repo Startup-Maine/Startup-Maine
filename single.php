7c8faf3f7c55e3f3f28e9f7c33eeeae4e6f45f16
Keeney <nkeeney@oceanics.io>
2024-04-01 09:48:31 -0400

    post page?

--- single.php
+++ single.php
@@ -8,10 +8,7 @@ the_post();
 		<div class="col-md-offset-1 col-md-10">
 			<div class="post">
 				<div class="row">
-					<div class="col-xs-4 col-sm-3">
-						<?php echo get_the_post_thumbnail($post->ID, 'large', array('class' => 'img-responsive'))?>
-					</div>
-					<div class="col-xs-8 col-sm-9">
+				<div>
 						<small><?php the_date('m.d.y')?></small>
 						<h4><?php the_title()?></h4>
 						<?php the_content()?>
