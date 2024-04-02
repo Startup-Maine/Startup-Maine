fa173a45ece98457cda0f8f823d09bccad5d42d9
Keeney <nkeeney@oceanics.io>
2024-04-02 03:59:27 -0400

    ditch js-cookies

--- vite.config.js
+++ vite.config.js
@@ -9,9 +9,8 @@ export default defineConfig({
             './node_modules/jquery/dist/jquery.min.js',
             './node_modules/jquery-ui-dist/jquery-ui.js',
             './node_modules/bootstrap/dist/js/bootstrap.min.js',
-            './node_modules/js-cookie/dist/js.cookie.js',
             './node_modules/lazysizes/lazysizes.min.js',
-            './resources/scripts/main.js',
+            '@/main.js',
         ]),
     ],
 });
