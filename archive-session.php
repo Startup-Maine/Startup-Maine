f7cfff71a244d87d24d6e9c17080f160f0ea21a0
Keeney <nkeeney@oceanics.io>
2024-05-07 10:10:54 -0400

    keep current

--- archive-session.php
+++ archive-session.php
@@ -7,7 +7,15 @@ $days = array(
 	'Wednesday' => array(),
 	'Thursday' => array(),
 	'Friday' => array(),
-    'Saturday' => array(),
+	'Saturday' => array(),
+);
+$dates = array(
+	'Monday' => 'May 13',
+	'Tuesday' => 'May 14',
+	'Wednesday' => 'May 15',
+	'Thursday' => 'May 16',
+	'Friday' => 'May 17',
+	'Saturday' => 'May 18',
 );
 $speakers = get_posts(array(
 	'post_type' => 'speaker',
@@ -16,13 +24,12 @@ $speakers = get_posts(array(
 $session_speakers = array();
 foreach ($speakers as $speaker) {
 	if ($sessions = get_field('sessions', $speaker)) {
-		foreach($sessions as $session) {
+		foreach ($sessions as $session) {
 			if (!isset($session_speakers[$session->ID])) $session_speakers[$session->ID] = array();
 			$session_speakers[$session->ID][] = '<a href="' . get_permalink($speaker) . '">' . get_the_title($speaker) . '</a>';
 		}
 	}
 }
-
 
 $sessions = get_posts(array(
 	'post_type' => 'session',
@@ -34,8 +41,6 @@ $sessions = get_posts(array(
 
 //loop through and group results into daily schedule 'slots'
 foreach ($sessions as $session) {
-	
-	//get custom field values
 	$session->day = get_field('day', $session);
 	$session->type = get_field('type', $session);
 	$session->start = get_field('start', $session);
@@ -43,63 +48,76 @@ foreach ($sessions as $session) {
 	$session->venue = get_field('venue', $session);
 	$session->link = get_permalink($session);
 	$session->speakers = isset($session_speakers[$session->ID]) ? implode(', ', $session_speakers[$session->ID]) : null;
-    $session->program_note = get_field('program_note', $session);
-	
-	//day not valid for some reason
+	$session->program_note = get_field('program_note', $session);
+
 	if (!array_key_exists($session->day, $days)) continue;
-	
-	//store info in key
 	$key = implode('|', array($session->start, $session->end, $session->type));
-	
-	//create slot for session
 	if (!array_key_exists($key, $days[$session->day])) $days[$session->day][$key] = array();
-	
-	//add session to slot
 	$days[$session->day][$key][] = $session;
-
 }
-//dd($days);
 ?>
-
+<link rel="preload" href="https://community.slicpix.com/js/embed/slicpix-embed-orchestrator.js" as="script" onload="var script = document.createElement('script');script.src = this.href;document.head.appendChild(script);">
+<link rel="preload" href="https://community.slicpix.com/js/embed/slicpix-embed-interact.js" as="script" onload="var script = document.createElement('script');script.src = this.href;document.head.appendChild(script);">
+<script language="javascript" type="text/javascript" src="https://community.slicpix.com/js/embed/slicpix-embed-primer.js" async></script>
 <div class="container" id="program">
-	
-        <h1>Maine Startup Week 2024</h1>
-        <p><em>Program schedule subject to change</em></p>
-			<a href="https://events.humanitix.com/maine-startup-week/tickets" style="border-radius:4px;background:#353337;font-family: Helvetica;font-size:16px;color:#FFFFFF;text-align:center;padding: 4px 18px;text-decoration: none;display: inline-block;">Purchase Week Pass</a>
-<div id="schedule">
+	<div id="metadata">
+		<h1>Startup Maine Week</h1>
+		<h3>May 14-17, 2024</h3>
+		<p>Join us for expert insight, skill building, and meaningful connections to make big things happen. Week passes are only $39 ($10 for students). Sales support programming by Startup Maine, a 501c3 organization.</p>
+		<p><em>Program schedule subject to change</em></p>
+		<a href="https://events.humanitix.com/maine-startup-week/tickets" style="border-radius:4px;background:#353337;font-family: Helvetica;font-size:16px;color:#FFFFFF;text-align:center;padding: 4px 18px;text-decoration: none;display: inline-block;">
+			Purchase Week Pass
+		</a>
+		<br/>
+		<div><a href="#map">Scroll to venue and session map</a></div>
+		<br/>
+		<div>
 			<?php foreach ($days as $day => $slots) {
 				if (empty($slots)) continue;
-				ksort($slots);
 				$key = sanitize_title($day);
-				?>
-				<h1 data-parent="#schedule" href="#<?php echo $key?>"><?php echo $day?></h1>
-				<div  id="<?php echo $key?>">
+			?>
+				<a href="#<?php echo $key ?>"><?php echo $day ?></a>
+			<?php } ?>
+		</div>
+		
+	</div>
+	<div id="schedule">
+		<?php foreach ($days as $day => $slots) {
+			if (empty($slots)) continue;
+			ksort($slots);
+			$key = sanitize_title($day);
+		?>
+			<h1><?php echo $day, ", ", $dates[$day] ?></h1>
+			<div id="<?php echo $key ?>">
 				<?php foreach ($slots as $key => $sessions) {
-					list ($start, $end, $type) = explode('|', $key);
-					?>
-				<div class="slot">
-					<div class="info">
-						<h3><?php echo mscw_time_range($start, $end)?></h3>
-						<?php if (!empty($type)) {?><small><?php echo $type?></small><?php }?>
-					</div>
-					<?php foreach ($sessions as $session) {?>
-						<div class="session <?php echo sanitize_title_with_dashes($session->type)?>">
-							<div class="inner">
-								<small><?php echo $session->venue?></small>
-								<a href="<?php echo $session->link?>" class="title"><?php echo $session->post_title?></a>
-                      			<small><?php echo $session->program_note?></small>
-								<?php echo $session->speakers?>
-							</div>
+					list($start, $end, $type) = explode('|', $key);
+				?>
+					<div class="slot">
+						<div class="info">
+							<h3><?php echo mscw_time_range($start, $end) ?></h3>
+							<?php if (!empty($type)) { ?><small><?php echo $type ?></small><?php } ?>
 						</div>
-					<?php }?>
-				</div>
-				<?php }?>
-				</div>
-			<?php 
-				$one_open = true;
-			}?>
+						<?php foreach ($sessions as $session) {
+							$external = str_contains($session->program_note, 'affil') || str_contains($session->program_note, 'Separate');
+							$bgcolor = $external ? 'background-color: #cccccc; !important' : '';
+						?>
+							<div class="session">
+								<div class="inner" style="<?php echo $bgcolor ?>">
+									<small><?php echo $session->venue ?></small>
+									<a href="<?php echo $session->link ?>" class="title"><?php echo $session->post_title ?></a>
+									<small><?php echo $session->program_note ?></small>
+									<?php echo $session->speakers ?>
+								</div>
+							</div>
+						<?php } ?>
+					</div>
+				<?php } ?>
 			</div>
+		<?php } ?>
+	</div>
+	<div id="map">
+		<img style="width:100%;" src="https://content.slicpix.com/messages/349877784232854248"></img>
+	</div>
 </div>
-
 <?php
 get_footer();
--- front-page.php
+++ front-page.php
@@ -7,68 +7,68 @@ get_header();
 		<div class="container">
 			<?php
 			if ($items = get_posts(array(
-					'post_type' => 'carousel',
-					'numberposts' => -1,
-				))) {?>
-			<div class="row">
-				<div class="col-md-offset-1 col-md-10">
-					<div class="carousel slide" id="home-carousel" data-ride="carousel">
-						<div class="carousel-inner">
-							<?php foreach ($items as $key => $item) {
-								setup_postdata($item);
+				'post_type' => 'carousel',
+				'numberposts' => -1,
+			))) { ?>
+				<div class="row">
+					<div class="col-md-offset-1 col-md-10">
+						<div class="carousel slide" id="home-carousel" data-ride="carousel">
+							<div class="carousel-inner">
+								<?php foreach ($items as $key => $item) {
+									setup_postdata($item);
 								?>
-							<div class="item<?php if (!$key) echo ' active'?>">
-								<?php if ($image = get_the_post_thumbnail_url($item->ID, 'large')) {?>
-									<div class="image" style="background-image:url(<?php echo $image?>)"></div>
-									<div class="caption">
-										<?php the_content()?>
+									<div class="item<?php if (!$key) echo ' active' ?>">
+										<?php if ($image = get_the_post_thumbnail_url($item->ID, 'large')) { ?>
+											<div class="image" style="background-image:url(<?php echo $image ?>)"></div>
+											<div class="caption">
+												<?php the_content() ?>
+											</div>
+										<?php } else {
+											the_content();
+										} ?>
 									</div>
-								<?php } else {
-									the_content();
-								}?>
+								<?php } ?>
 							</div>
-							<?php }?>
+							<?php if (count($items) > 1) { ?>
+								<a class="left carousel-control" href="#home-carousel" role="button" data-slide="prev">
+									<span class="fa fa-angle-left" aria-hidden="true"></span>
+									<span class="sr-only">Previous</span>
+								</a>
+								<a class="right carousel-control" href="#home-carousel" role="button" data-slide="next">
+									<span class="fa fa-angle-right" aria-hidden="true"></span>
+									<span class="sr-only">Next</span>
+								</a>
+							<?php } ?>
 						</div>
-						<?php if (count($items) > 1) {?>
-						<a class="left carousel-control" href="#home-carousel" role="button" data-slide="prev">
-							<span class="fa fa-angle-left" aria-hidden="true"></span>
-							<span class="sr-only">Previous</span>
-						</a>
-						<a class="right carousel-control" href="#home-carousel" role="button" data-slide="next">
-							<span class="fa fa-angle-right" aria-hidden="true"></span>
-							<span class="sr-only">Next</span>
-						</a>
-						<?php }?>
 					</div>
 				</div>
-			</div>
-			<?php }?>
+			<?php } ?>
 			<div class="row">
 				<div class="col-md-offset-1 col-md-10">
-<!--					<div class="tagline intro">-->
-<!--						<h1 class="giant">Join us in 2020.</h1>-->
-<!--                        <p class="large">Join us for three days of expert insight, applied skill-building, and meaningful connections that will help make big things happen. <strong>June 17th - 19th 2020</strong></p>-->
-<!--                        <p class="large">New location this year! <strong>University of Southern Maine Abromson Center, Portland</strong></p>-->
-<!--						<p class="button"><a href="https://www.eventbrite.com/e/startup-maine-2020-tickets-93013496873" target="_blank">Buy Tickets</a></p>-->
-<!--					</div>-->
-<!--                    <div class="tagline call-to-action">-->
-<!--                        <h2>Partner with us.  Get in touch!</h2>-->
-<!--                        <p>Want to be a sponsor of Startup Maine? <br />Learn how you can get involved.</p>-->
-<!--                        <p class="button2"><a href="https://www.startupmaine.org/become-a-sponsor">Reach Out</a></p>-->
-<!--                    </div>-->
-<!---->
-<!--                    <div class="tagline call-to-action">-->
-<!--                        <h2>Entrepreneurship Guide</h2>-->
-<!--                        <p>Are you an entrepreneur or startup-curious person who wants help navigating the Maine ecosystem? Check out this resource guide:</p>-->
-<!--                        <p class="button2"><a href="https://www.google.com/url?q=https://www.startupmaine.org/wp-content/uploads/2019/11/A-Guide-to-Resources-for-Maine-Entrepreneurs-5th-Edition-November-2019.pdf&sa=D&source=hangouts&ust=1573587947592000&usg=AFQjCNHr5MvMYrn01bx2vKekDoq6qb7v0A">Guide</a></p>-->
-<!--                    </div>-->
+					<!--					<div class="tagline intro">-->
+					<!--						<h1 class="giant">Join us in 2020.</h1>-->
+					<!--                        <p class="large">Join us for three days of expert insight, applied skill-building, and meaningful connections that will help make big things happen. <strong>June 17th - 19th 2020</strong></p>-->
+					<!--                        <p class="large">New location this year! <strong>University of Southern Maine Abromson Center, Portland</strong></p>-->
+					<!--						<p class="button"><a href="https://www.eventbrite.com/e/startup-maine-2020-tickets-93013496873" target="_blank">Buy Tickets</a></p>-->
+					<!--					</div>-->
+					<!--                    <div class="tagline call-to-action">-->
+					<!--                        <h2>Partner with us.  Get in touch!</h2>-->
+					<!--                        <p>Want to be a sponsor of Startup Maine? <br />Learn how you can get involved.</p>-->
+					<!--                        <p class="button2"><a href="https://www.startupmaine.org/become-a-sponsor">Reach Out</a></p>-->
+					<!--                    </div>-->
+					<!---->
+					<!--                    <div class="tagline call-to-action">-->
+					<!--                        <h2>Entrepreneurship Guide</h2>-->
+					<!--                        <p>Are you an entrepreneur or startup-curious person who wants help navigating the Maine ecosystem? Check out this resource guide:</p>-->
+					<!--                        <p class="button2"><a href="https://www.google.com/url?q=https://www.startupmaine.org/wp-content/uploads/2019/11/A-Guide-to-Resources-for-Maine-Entrepreneurs-5th-Edition-November-2019.pdf&sa=D&source=hangouts&ust=1573587947592000&usg=AFQjCNHr5MvMYrn01bx2vKekDoq6qb7v0A">Guide</a></p>-->
+					<!--                    </div>-->
 
 
-                    <div class="row testimonials">
+					<div class="row testimonials">
 						<div class="col-md-6 testimonial">
 							<div class="row">
 								<div class="col-xs-6">
-									<img data-src="<?php echo get_stylesheet_directory_uri()?>/assets/img/testimonials/01-interview.jpg" width="1500" height="1000" class="img-responsive lazyload">
+									<img data-src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/testimonials/01-interview.jpg" width="1500" height="1000" class="img-responsive lazyload">
 								</div>
 								<div class="col-xs-6">
 									“The variety of workshop sessions was great, I wish I could have gone to all of them. The keynote speakers during lunch were also very informative. I felt like my time was used wisely during MSCW.”
@@ -78,7 +78,7 @@ get_header();
 						<div class="col-md-6 testimonial">
 							<div class="row">
 								<div class="col-xs-6">
-									<img data-src="<?php echo get_stylesheet_directory_uri()?>/assets/img/testimonials/02-panel.jpg" width="1500" height="1000" class="img-responsive lazyload">
+									<img data-src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/testimonials/02-panel.jpg" width="1500" height="1000" class="img-responsive lazyload">
 								</div>
 								<div class="col-xs-6">
 									“It was not academic; the guest speakers are out there doing it! Very upbeat atmosphere.”
@@ -90,7 +90,7 @@ get_header();
 			</div>
 		</div>
 	</div>
-		
+
 	<div class="sponsors">
 		<div class="container">
 			<div class="row">
@@ -105,10 +105,10 @@ get_header();
 						'Gold' => array(),
 						'Silver' => array(),
 						'Lightning' => array(),
-                        'Presenting Media Sponsor' => array(),
-                        'Business News Media Sponsor'  => array(),
-                        'Partner Donors' => array(),
-                        'Magazine Sponsor' => array(),
+						'Presenting Media Sponsor' => array(),
+						'Business News Media Sponsor'  => array(),
+						'Partner Donors' => array(),
+						'Magazine Sponsor' => array(),
 					);
 					$sponsors = get_posts(array(
 						'post_type' => 'sponsor',
@@ -125,27 +125,38 @@ get_header();
 					}
 					foreach ($tiers as $tier => $sponsors) {
 						if (empty($sponsors)) continue;
-						?>
+					?>
 						<div class="tier">
-							<small><?php echo $tier?></small>
+							<small><?php echo $tier ?></small>
 							<div class="row"><!--
 							<?php foreach ($sponsors as $sponsor) {
 								$imagesrc =  array("src=", "srcset=");
 								$imagesetsrc =  array("data-src=", "data-srcset=");
-								?>
-								--><div class="col-xs-12 col-md-3 <?php echo $tier?>"><a href="<?php echo $sponsor['url']?>" title="<?php echo $sponsor['title']?>" target="_blank"><?php echo str_replace($imagesrc, $imagesetsrc, $sponsor['image'])?></a></div><!--
-							<?php }?>
+							?>
+								-->
+								<div class="col-xs-12 col-md-3 <?php echo $tier ?>"><a href="<?php echo $sponsor['url'] ?>" title="<?php echo $sponsor['title'] ?>" target="_blank"><?php echo str_replace($imagesrc, $imagesetsrc, $sponsor['image']) ?></a></div><!--
+							<?php } ?>
 							<?php
-						?>
-							--></div>
+							?>
+							-->
+							</div>
 						</div>
-					<?php }?>
+					<?php } ?>
 				</div>
 			</div>
 		</div>
 	</div>
-	<script async defer type="text/javascript" src="//downloads.mailchimp.com/js/signup-forms/popup/embed.js" data-dojo-config="usePlainJson: true, isDebug: false"></script><script type="text/javascript">require(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us12.list-manage.com","uuid":"e622297fee792d99c7d2d1d37","lid":"22f8f595d4"}) })</script>
+	<script async defer type="text/javascript" src="//downloads.mailchimp.com/js/signup-forms/popup/embed.js" data-dojo-config="usePlainJson: true, isDebug: false"></script>
+	<script type="text/javascript">
+		require(["mojo/signup-forms/Loader"], function(L) {
+			L.start({
+				"baseUrl": "mc.us12.list-manage.com",
+				"uuid": "e622297fee792d99c7d2d1d37",
+				"lid": "22f8f595d4"
+			})
+		})
+	</script>
 </div>
 
-<?php 
+<?php
 get_footer();
